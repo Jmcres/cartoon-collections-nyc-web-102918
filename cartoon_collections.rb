2595dcfe9713e06d9d172b044f40659846afe004
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-15 18:40:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,11 +8,12 @@ def summon_captain_planet (name)
 end
 
 def long_planeteer_calls(short_words)
-  short_words.any? # code an argument here
+  short_words.any? { |i| i.length > 4 } # code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (type) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  type.find { |i| cheese_type.include?(i)}
 end
